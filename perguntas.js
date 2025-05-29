5121aa8bad55e7bfd73ee2a12ca1580f593ee46e
tyler21pilots <shantal.cardozo@escola.pr.gov.br>
2025-05-29 09:18:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lingua Espanhola',
+    'Como se diz obrigado/a em espanhol?',
+    'Obrigado/a em espanhol se diz GRACIAS'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Venezuela?',
+    'A capital da Venezuela é Caracas'
+)
+
+criaCartao(
+    'Em qual Cristobal Colombo descubriu america?',
+    'Descubriu america no ano 1492',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'Gato em ingles é CAT'
+)
